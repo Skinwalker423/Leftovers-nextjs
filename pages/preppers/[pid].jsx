21428db54020bb041f0959b3fd903c0a568b938d
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-12-14 09:24:25 -0800

    getStaticProps

--- pages/preppers/[pid].jsx
+++ pages/preppers/[pid].jsx
@@ -1,11 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Box, Typography, CircularProgress } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import NavBar from '../../components/global/NavBar';
 import CustomLoader from '../../components/Loader';
+import { mockDataContacts } from '../../data/mockData';
 
-const Prepper = () => {
+export async function getStaticProps({ params }) {
+	const prepperId = params.pid;
+	console.log(prepperId);
+	// const videoList = await getVideoById(vidId);
+	const findPrepper = mockDataContacts.find(
+		(prepper) => prepperId == prepper.id
+	);
+	console.log(findPrepper);
+
+	return {
+		props: {
+			prepper: findPrepper ? findPrepper : [],
+		},
+
+		revalidate: 60, // In seconds
+	};
+}
+
+export async function getStaticPaths() {
+	const listofBannerKitchens = ['1', '2', '3'];
+
+	const paths = listofBannerKitchens.map((prepperId) => ({
+		params: { pid: prepperId },
+	}));
+
+	return { paths, fallback: 'blocking' };
+}
+
+const Prepper = ({ prepper }) => {
 	const router = useRouter();
+	console.log(prepper);
 
 	const prepperId = router.query.pid;
 	if (!prepperId) {
@@ -14,14 +44,19 @@ const Prepper = () => {
 
 	return (
 		<Box
-			m='20px'
 			display='flex'
 			justifyContent='center'
 			alignItems='center'
 			width={'100%'}
 			height='100%'>
 			<NavBar />
-			<Typography variant='h1'>Prepper id: {prepperId}</Typography>
+			<Box
+				m='100px 0'
+				display='flex'
+				justifyContent='center'
+				alignItems='center'>
+				<Typography variant='h1'>Prepper id: {prepperId}</Typography>
+			</Box>
 		</Box>
 	);
 };
--- pages/preppers/index.jsx
+++ pages/preppers/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import NavBar from '../../components/global/NavBar';
 import { Box, Typography } from '@mui/material';
 import PrepperCard from '../../components/Card';
+import Link from 'next/link';
+import { mockDataContacts } from '../../data/mockData';
 
 const Home = () => {
 	return (
@@ -15,12 +17,19 @@ const Home = () => {
 			<NavBar />
 			<Typography variant='h1'>List of all preppers in your area</Typography>
 			<Box mt='20px' display='flex' gap='10px' flexWrap={'wrap'}>
-				<PrepperCard />
-				<PrepperCard />
-				<PrepperCard />
-				<PrepperCard />
-				<PrepperCard />
-				<PrepperCard />
+				{mockDataContacts.map((prepper) => {
+					const avatar = 'https://i.pravatar.cc/300';
+					return (
+						<Link key={prepper.id} href={`/preppers/${prepper.id}`}>
+							<PrepperCard
+								title={prepper.name}
+								subTitle={prepper.email}
+								avatar={avatar}
+								id={prepper.id}
+							/>
+						</Link>
+					);
+				})}
 			</Box>
 		</Box>
 	);
