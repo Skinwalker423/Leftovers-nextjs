2896b09f92fada47d0c39693a7403f9716a2e6e5
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-12-16 09:39:05 -0800

    todo component

---
+++ components/toDoList/index.js
@@ -0,0 +1,95 @@
+import React, { useState, useRef } from 'react';
+import FormDialog from '../modal/ToDoList';
+import {
+	Box,
+	MenuList,
+	MenuItem,
+	Typography,
+	IconButton,
+	Paper,
+} from '@mui/material';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import EditIcon from '@mui/icons-material/Edit';
+import { useColors } from '../../hooks';
+
+const mockToDoList = [
+	'buy grocieries',
+	'pay bill',
+	'doctor appointment',
+	'gym',
+];
+
+const ToDoList = () => {
+	const [currentIndex, setCurrentIndex] = useState(null);
+	const [toDoList, setToDoList] = useState(mockToDoList);
+	const eventRef = useRef();
+	const { colors } = useColors();
+
+	const handleSelected = (index) => {
+		setCurrentIndex(index);
+	};
+	const handleRemoveToDo = () => {
+		const newTodo = toDoList.filter((event, index) => currentIndex !== index);
+		setToDoList(newTodo);
+		setCurrentIndex(null);
+	};
+	const handleEdit = () => {};
+
+	return (
+		<Box
+			display='flex'
+			flexDirection='column'
+			justifyContent='center'
+			alignItems='center'
+			height='100vh'>
+			<Box width='500px'>
+				<Paper>
+					<Box
+						display='flex'
+						alignItems='center'
+						padding='20px'
+						borderBottom='2px solid orange'
+						justifyContent='space-between'>
+						<Typography variant='h1'>To Do List</Typography>
+						<Box>
+							<FormDialog setToDoList={setToDoList} />
+						</Box>
+					</Box>
+					<MenuList>
+						{toDoList.map((event, index) => {
+							return (
+								<MenuItem
+									key={'event' + index}
+									selected={index === currentIndex}
+									onClick={() => {
+										handleSelected(index);
+									}}
+									sx={{
+										display: 'flex',
+										justifyContent: 'space-between',
+										height: '50px',
+									}}>
+									<Typography>
+										{index + 1}: {event}
+									</Typography>
+									{index === currentIndex && (
+										<Box>
+											<IconButton onClick={handleEdit}>
+												<EditIcon />
+											</IconButton>
+											<IconButton onClick={handleRemoveToDo}>
+												<DeleteOutlineIcon />
+											</IconButton>
+										</Box>
+									)}
+								</MenuItem>
+							);
+						})}
+					</MenuList>
+				</Paper>
+			</Box>
+		</Box>
+	);
+};
+
+export default ToDoList;
