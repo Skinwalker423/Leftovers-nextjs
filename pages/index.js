38b831e99d92f4fdb71c56f2a545b9db180124dd
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-12-29 13:28:48 -0800

    optimize mock data

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,6 @@ export default function Home() {
 			<main className={styles.main}>
 				<NavBar />
 				<LandingHeader title='Welcome to Leftovers!' img='ball-park.jpg' />
-
 				<FindLocalPreppersSearchBar
 					handleZipChange={handleZipChange}
 					handleZipSearchForm={handleZipSearchForm}
