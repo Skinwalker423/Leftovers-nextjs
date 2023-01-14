f5052a3255b9ceb0ff7ce951920d4f4b80346ef6
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-01-14 14:18:54 -0800

    favorites page Head, main, and Footer

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import FavoriteList from '../components/favorites/favoriteList';
-import { mockDataContacts } from '../data/mockData';
 import fetchFavoritePreppers from '../utils/fetchFavoritePreppers';
+import Head from 'next/head';
+import { Box } from '@mui/material';
+import styles from '/styles/Home.module.css';
+import Footer from '../components/footer/footer';
 
 export async function getServerSideProps() {
 	const favoritePreppersList = await fetchFavoritePreppers();
@@ -13,7 +16,20 @@ export async function getServerSideProps() {
 }
 
 const Favorites = ({ favoriteList }) => {
-	return <FavoriteList favoriteList={favoriteList} />;
+	return (
+		<Box className={styles.container}>
+			<Head>
+				<title>Favorites</title>
+				<meta name='description' content='Your favorite preppers' />
+			</Head>
+			<main>
+				<FavoriteList favoriteList={favoriteList} />
+			</main>
+			<footer className={styles.footer}>
+				<Footer img={'/icons8-connect.svg'} title='Leftovers' />
+			</footer>
+		</Box>
+	);
 };
 
 export default Favorites;
