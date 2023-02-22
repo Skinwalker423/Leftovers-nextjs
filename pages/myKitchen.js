eac78dbc7d43139ff57916046dfc090557680ec6
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-02-22 15:50:33 -0800

    default avatar

--- pages/myKitchen.js
+++ pages/myKitchen.js
@@ -5,7 +5,8 @@ import { Box, Typography, Alert } from '@mui/material';
 import Image from 'next/image';
 import MyKitchenForm from '../components/UI/form/mykitchen/myKitchenForm';
 import RegistrationForm from '../components/UI/form/registration/registrationForm';
-import AddMeal from '../components/UI/modal/addMeal';
+import AddMeal from '../components/UI/form/mykitchen/addMeal';
+import DefaultAvatar from '../components/UI/icon/defaultAvatar';
 import {
 	connectMongoDb,
 	findExistingPrepperEmail,
@@ -36,7 +37,7 @@ export async function getServerSideProps({ req, res }) {
 
 const myKitchen = ({ userData, prepper }) => {
 	console.log(prepper);
-	const { name = 'User', email, image = '/icons8-connect.svg' } = userData;
+	const { name = 'User', email, image } = userData;
 	return (
 		<Box
 			width='100%'
@@ -46,6 +47,21 @@ const myKitchen = ({ userData, prepper }) => {
 			justifyContent='space-around'
 			alignItems={'center'}>
 			<Box>
+				{image ? (
+					<Image
+						alt={`avatar image of ${name}`}
+						src={image}
+						width={100}
+						height={100}
+						priority
+					/>
+				) : (
+					<DefaultAvatar
+						userEmail={prepper.name}
+						widthHeight={100}
+						fontSize='3em'
+					/>
+				)}
 				<Typography variant='h1'>{prepper.kitchenTitle}</Typography>
 				<Typography variant='h2'>{email}</Typography>
 				<Typography variant='h3'>{prepper.description}</Typography>
@@ -56,16 +72,9 @@ const myKitchen = ({ userData, prepper }) => {
 				<Typography variant='h3'>
 					zipcode: {prepper ? prepper.location.zipcode : ''}
 				</Typography>
-				<Typography variant='h3'>Pic</Typography>
-				<Image
-					alt={`avatar image of ${name}`}
-					src={image}
-					width={100}
-					height={100}
-					priority
-				/>
+
+				<AddMeal />
 			</Box>
-			<AddMeal />
 			{!prepper && (
 				<MyKitchenForm sessionEmail={email} title='Register Kitchen' />
 			)}
