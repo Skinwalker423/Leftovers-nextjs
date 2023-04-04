ed582c96944b2fe4b6b2317336ffb37db0b8a1db
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-04-03 20:39:17 -0700

    added category banner

--- components/mealLists/valueMealList.js
+++ components/mealLists/valueMealList.js
@@ -1,20 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, use } from 'react';
 import FoodItemCard from '../Card/foodItemCard';
+import Box from '@mui/material/Box';
+import { RouterTwoTone } from '@mui/icons-material';
 
-const ValueMealList = ({ localPreppers, setMessage }) => {
+const ValueMealList = ({ localPreppers, setMessage, row }) => {
 	console.log(localPreppers);
 	const [valueList, setValueList] = useState([]);
 
-	// for (let prepper in localPreppers) {
-	// 	if (prepper.meals) {
-	// 		const filteredList = prepper.meals.filter((meal) => meal.price === 5);
-	// 		setValueList([...valueList, ...filteredList]);
-	// 	}
-	// }
+	if (!localPreppers.length) {
+		return 'loading...';
+	}
 
-	// console.log(valueList);
+	const list = localPreppers.map((prepper) => {
+		return prepper.meals.map(({ id, title, price, qty, description }) => {
+			if (price === 5 && qty > 0) {
+				return (
+					<FoodItemCard
+						prepperEmail={prepper.email}
+						foodItem={title}
+						id={id}
+						price={price}
+						qty={qty}
+						description={description}
+					/>
+				);
+			}
+		});
+	});
 
-	return <div>list</div>;
+	console.log(list);
+
+	return (
+		<Box
+			display={'flex'}
+			flexWrap={{ xs: 'unset', md: 'wrap' }}
+			width={'100%'}
+			gap='3em'
+			mb={'1em'}
+			sx={{
+				height: {
+					xs: row ? '' : '30rem',
+					sm: row ? '' : '30rem',
+					md: '',
+					lg: '',
+				},
+				overflowY: 'auto',
+			}}>
+			{list}
+		</Box>
+	);
 };
 
 export default ValueMealList;
