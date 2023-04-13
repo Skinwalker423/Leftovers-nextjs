ebeac8a0d4e0d5e3a365d08f591ec29b90f06827
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-04-12 17:45:42 -0700

    show favorited icon filled in localpreppers

--- components/prepperLists/localPreppersList.js
+++ components/prepperLists/localPreppersList.js
@@ -7,12 +7,18 @@ import styles from './localPreppersList.module.css';
 const LocalPreppersList = ({ userEmail, setMsg }) => {
 	const { state } = useContext(UserContext);
 
+	const favoritesPrepId = state.favorites.map(({ id }) => id);
+
 	const preppers = state.localPreppers.map(
 		({ id, description, kitchenTitle, email }) => {
+			const favorited =
+				state.favorites && favoritesPrepId.includes(id) ? true : false;
+
 			const avatar = 'https://i.pravatar.cc/300';
 			if (email !== userEmail) {
 				return (
 					<PrepperCard
+						isFavorited={favorited}
 						name={kitchenTitle}
 						avatar={avatar}
 						id={id}
