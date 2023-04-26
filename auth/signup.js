0fa3a2d20a61796a6773629532b577920150b946
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-04-25 19:27:15 -0700

    background image and form styles

---
+++ auth/signup.js
@@ -0,0 +1,184 @@
+import React, { useRef, useState } from 'react';
+import {
+	Box,
+	Paper,
+	TextField,
+	Typography,
+	Button,
+	Alert
+} from '@mui/material';
+import { validateEmail } from '../../../../utils/form-validation';
+import { signIn } from 'next-auth/react';
+import { useColors } from '../../../../hooks/useColors';
+
+const SignUpForm = ({ title }) => {
+	const { colors } = useColors();
+	const [errorMsg, setErrorMsg] = useState('');
+	const [msg, setMsg] = useState('');
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const confirmPasswordRef = useRef();
+
+	const handleSignUpFormSubmit = async (e) => {
+		e.preventDefault();
+		console.log('registration pending');
+		setErrorMsg('');
+		setMsg('');
+		const email = emailRef.current.value;
+		const password = passwordRef.current.value;
+		const confirmPassword = confirmPasswordRef.current.value;
+
+		if (password !== confirmPassword) {
+			setErrorMsg('password does not match');
+			return;
+		}
+
+		const isValidEmail = validateEmail(email);
+
+		if (!isValidEmail) {
+			setErrorMsg('Invalid email');
+			return;
+		}
+
+		const formBody = {
+			email: emailRef.current.value,
+			password: passwordRef.current.value,
+			confirmPassword: confirmPasswordRef.current.value
+		};
+		try {
+			const response = await fetch('/api/register/user', {
+				headers: {
+					'Content-type': 'application/json'
+				},
+				method: 'POST',
+				body: JSON.stringify(formBody)
+			});
+			const data = await response.json();
+			console.log(data);
+
+			if (data.error) {
+				setErrorMsg(data.error);
+			} else {
+				setMsg(data.message);
+				signIn();
+			}
+		} catch (err) {
+			setErrorMsg(err);
+		}
+	};
+
+	return (
+		<Paper
+			sx={{
+				width: {
+					xs: '100%',
+					md: '45%'
+				},
+				height: {
+					xs: '50vh'
+				},
+				my: '1em',
+				boxShadow: 5,
+				border: `2px solid ${colors.orangeAccent[900]}`
+			}}
+		>
+			<Typography pt={'.5em'} textAlign="center" variant="h1">
+				Sign Up
+			</Typography>
+			<Box
+				width={'100%'}
+				sx={{
+					px: {
+						xs: '2em',
+						md: '5em',
+						lg: '8em'
+					}
+				}}
+			>
+				<form onSubmit={handleSignUpFormSubmit}>
+					<Box width="100%" mt="1em">
+						<TextField
+							id="email"
+							type="email"
+							label="Email"
+							required
+							autoComplete="on"
+							inputRef={emailRef}
+							color="secondary"
+							fullWidth
+						/>
+					</Box>
+					<Box width="100%" mt="1em">
+						<TextField
+							id="password"
+							type="password"
+							label="Password"
+							required
+							autoComplete="on"
+							inputRef={passwordRef}
+							color="secondary"
+							fullWidth
+						/>
+					</Box>
+					<Box width="100%" mt="1em">
+						<TextField
+							id="confirmPassword"
+							type="password"
+							label="Confirm Password"
+							required
+							autoComplete="on"
+							inputRef={confirmPasswordRef}
+							color="secondary"
+							fullWidth
+						/>
+					</Box>
+					<Box>
+						<Button
+							sx={{ mt: '3rem', height: '4em' }}
+							variant="contained"
+							fullWidth
+							color="success"
+							type="submit"
+							required
+							size="large"
+						>
+							<Typography fontSize={'large'}>Submit</Typography>
+						</Button>
+					</Box>
+				</form>
+			</Box>
+			<Box
+				display={'flex'}
+				width={'100%'}
+				justifyContent="center"
+				alignItems="center"
+				mt="3em"
+			>
+				{msg && (
+					<Alert
+						sx={{
+							width: '50%',
+							fontSize: 'larger'
+						}}
+						severity="success"
+					>
+						{msg}
+					</Alert>
+				)}
+				{errorMsg && (
+					<Alert
+						sx={{
+							width: '50%',
+							fontSize: 'larger'
+						}}
+						severity="error"
+					>
+						{errorMsg}
+					</Alert>
+				)}
+			</Box>
+		</Paper>
+	);
+};
+
+export default SignUpForm;
---
+++ signin.js
@@ -0,0 +1,63 @@
+import { getSession } from 'next-auth/react';
+import React from 'react';
+import { Box } from '@mui/material';
+import SignIn from '../components/UI/form/auth/signin';
+import SignUpForm from '../components/UI/form/auth/signup';
+import Head from 'next/head';
+
+export async function getServerSideProps({ req }) {
+	const session = await getSession({ req });
+	console.log('checking session', session);
+
+	if (session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false
+			}
+		};
+	}
+
+	return {
+		props: {}
+	};
+}
+
+const signin = () => {
+	return (
+		<Box
+			sx={{ backgroundImage: 'url("./art.jpg")', backgroundSize: 'cover' }}
+			display={'flex'}
+			justifyContent="center"
+			alignItems={'center'}
+			width="100%"
+			height="100vh"
+		>
+			<Head>
+				<title>Sign In</title>
+				<meta
+					name="description"
+					content="Sign in using your email and password, or by signing in with Google"
+				/>
+			</Head>
+			<Box
+				width={'90%'}
+				display={'flex'}
+				justifyContent="space-around"
+				alignItems={'center'}
+				sx={{
+					flexDirection: {
+						sm: 'column',
+						xs: 'column',
+						md: 'row'
+					}
+				}}
+			>
+				<SignIn />
+				<SignUpForm />
+			</Box>
+		</Box>
+	);
+};
+
+export default signin;
