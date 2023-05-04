da927d25255b7c52545c04a647a201b34f2fb03c
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-05-03 21:23:40 -0700

    landing page design

--- components/layout/header/landingHeader.js
+++ components/layout/header/landingHeader.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Image from 'next/image';
 import styles from './landingPage.module.css';
+import Stack from '@mui/material/Stack';
 
 const LandingHeader = ({ title = 'Header goes here', img = '/art.jpg' }) => {
 	return (
-		<Box position={'relative'} top="0" height={'75vh'} width="100%">
-			<Image priority={true} fill src={img} alt="landing page image" />
-			<Typography
-				variant="h1"
-				className={styles.title}
-				lineHeight={1.15}
-				fontSize="4rem"
-				color="white"
+		<Box position={'relative'} top="0" height={'85vh'} width="100%">
+			<Image
+				priority={true}
+				fill
+				src={img}
+				alt="landing page image of various popular American foods"
+			/>
+			<Stack
+				position={'relative'}
+				alignItems={'center'}
+				justifyContent={'center'}
+				top={'15%'}
+				p={'1rem'}
 			>
-				{title}
-			</Typography>
+				<Typography
+					variant="h1"
+					className={styles.title}
+					lineHeight={1.5}
+					fontWeight={900}
+					fontSize="4rem"
+					color="white"
+					textAlign={'center'}
+				>
+					{title}
+				</Typography>
+				<Typography
+					textAlign={'center'}
+					lineHeight={1.5}
+					fontSize={'large'}
+					color={'white'}
+				>
+					The largest food sharing platform, where you can find local authentic
+					meals being prepared by the community and shared to the community
+				</Typography>
+			</Stack>
 		</Box>
 	);
 };
