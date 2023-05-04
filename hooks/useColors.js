8d1e01dccbc5c8fa36798673c49b5664efb4f432
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-05-03 22:02:51 -0700

    Delete hooks directory

--- hooks/useColors.js
+++
@@ -1,9 +0,0 @@
-import { tokens } from '../config/theme';
-import { useTheme } from '@mui/material';
-
-export const useColors = () => {
-	const { palette } = useTheme();
-	const colors = tokens(palette.mode);
-
-	return { colors, palette };
-};
