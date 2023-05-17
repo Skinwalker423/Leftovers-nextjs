c80aaf3c25593febb6e833ca1e83fe6ca319ca61
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-05-16 20:11:32 -0700

    Head tag and getStaticProps

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Head from 'next/head';
+
+export async function getStaticProps() {
+	return {
+		props: {}
+	};
+}
 
 const About = () => {
-	return <div>About</div>;
+	return (
+		<Box>
+			<Head>
+				<title>About Us</title>
+				<meta
+					name="description"
+					content="Leftovers is dedicated to providing a platform that allows neighbors to share their excess meals with others in the community"
+				/>
+			</Head>
+		</Box>
+	);
 };
 
 export default About;
