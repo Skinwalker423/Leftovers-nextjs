f601d511abf32c38f7d5140c72af605bde0e54f5
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-05-17 20:00:41 -0700

    Mission statement body

--- pages/about.js
+++ pages/about.js
@@ -46,6 +46,34 @@ const About = () => {
 				</Box>
 			</header>
 			<main>
+				<Box
+					m={5}
+					display={'flex'}
+					justifyContent={'center'}
+					mx={'5rem'}
+					textAlign={'center'}
+					textOverflow={'clip'}
+				>
+					<Typography fontSize={'large'} component={'p'} sx={{ width: '50em' }}>
+						Velit sint laboris labore laboris dolore aute veniam quis. Id ex
+						pariatur incididunt consequat sunt ullamco id consequat. Adipisicing
+						qui cupidatat ipsum aliquip. Consectetur id ipsum anim ipsum do
+						laborum ad commodo ea cupidatat aute laborum consequat dolor. Est
+						aliquip ipsum magna excepteur sit duis occaecat ea cillum. Id nisi
+						elit reprehenderit aute deserunt ullamco cillum do consectetur irure
+						do. Cillum qui non amet ea elit ut. Occaecat qui aliqua non labore
+						in eiusmod reprehenderit. Ex magna nisi dolore culpa nisi veniam
+						aliqua duis. Consectetur quis do tempor sint consequat consectetur
+						dolor. Laboris id anim est sunt nulla sunt aliqua dolor eiusmod
+						tempor magna nulla quis duis. Est cupidatat duis ea nisi duis velit
+						cillum magna nostrud mollit do mollit. Dolor duis occaecat elit nisi
+						nulla amet enim commodo quis tempor. Nisi et mollit consectetur
+						incididunt laborum eu consectetur irure labore ex laboris dolore
+						magna fugiat. Irure commodo quis cupidatat officia officia
+						cupidatat. Dolore nisi officia voluptate incididunt cillum fugiat ad
+						fugiat labore nulla culpa consectetur exercitation.
+					</Typography>
+				</Box>
 				<Box>
 					<PromoSection />
 					<PromoSection reverse />
