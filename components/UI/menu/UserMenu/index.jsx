37ab538eacfef99da3ff08efff86364ef1214809
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-05-21 21:03:02 -0700

    icons and links

--- components/UI/menu/UserMenu/index.jsx
+++ components/UI/menu/UserMenu/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {
-	Box,
-	Typography,
-	MenuList,
-	MenuItem,
-	Paper,
-	Divider
-} from '@mui/material';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import Paper from '@mui/material/Paper';
+import Divider from '@mui/material/Divider';
 import { useColors } from '../../../../hooks/useColors';
 import { signOut } from 'next-auth/react';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import DefaultAvatar from '../../icon/defaultAvatar';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const UserMenu = () => {
 	const { colors } = useColors();
@@ -46,7 +47,7 @@ const UserMenu = () => {
 				flexDirection="column"
 				justifyContent={'center'}
 				alignItems="center"
-				m={'20px 0'}
+				m={'1.5em 0'}
 			>
 				{userIcon ? (
 					<Box>
@@ -67,22 +68,52 @@ const UserMenu = () => {
 			</Box>
 			<Divider flexItem variant="middle" color={colors.orangeAccent[900]} />
 			<MenuList>
-				<MenuItem sx={{ height: '4em', justifyContent: 'center' }}>
-					Profile
-				</MenuItem>
-				<Link
-					style={{ textDecoration: 'none', color: colors.primary[100] }}
-					href={'/messages'}
-				>
-					<MenuItem sx={{ height: '4em', justifyContent: 'center' }}>
-						Messages
+				<Link style={{ textDecoration: 'none' }} href={'/'}>
+					<MenuItem
+						sx={{
+							height: '4em',
+							pl: '2.5em',
+							justifyContent: 'flex-start',
+							alignItems: 'center',
+							gap: 5
+						}}
+					>
+						<AccountCircle fontSize="large" color="secondary" />
+						<Typography color={colors.primary[100]} fontSize={'large'}>
+							Profile
+						</Typography>
+					</MenuItem>
+				</Link>
+				<Link style={{ textDecoration: 'none' }} href={'/messages'}>
+					<MenuItem
+						sx={{
+							height: '4em',
+							pl: '2.5em',
+							justifyContent: 'flex-start',
+							alignItems: 'center',
+							gap: 5
+						}}
+					>
+						<MailIcon fontSize="large" color="secondary" />
+						<Typography color={colors.primary[100]} fontSize={'large'}>
+							Messages
+						</Typography>
 					</MenuItem>
 				</Link>
 				<MenuItem
-					sx={{ height: '4em', justifyContent: 'center' }}
 					onClick={handleSignOut}
+					sx={{
+						height: '4em',
+						pl: '2.5em',
+						justifyContent: 'flex-start',
+						alignItems: 'center',
+						gap: 5
+					}}
 				>
-					Logout
+					<LogoutIcon fontSize="large" color="secondary" />
+					<Typography color={colors.primary[100]} fontSize={'large'}>
+						Sign Out
+					</Typography>
 				</MenuItem>
 			</MenuList>
 		</Paper>
