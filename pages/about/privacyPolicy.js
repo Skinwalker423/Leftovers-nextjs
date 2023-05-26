f1759efa7fea268e58e49f57fd0c7901e3262218
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-05-25 21:11:37 -0700

    PrivacyStatement component

---
+++ pages/about/privacyPolicy.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Head from 'next/head';
+import Footer from '../../components/layout/footer/footer';
+import styles from '../../styles/Home.module.css';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import PrivacyStatement from '../../components/policies/privacyStatement';
+import { useColors } from '../../hooks/useColors';
+
+export async function getStaticProps() {
+	return {
+		props: {}
+	};
+}
+
+const PrivacyPolicy = () => {
+	const { colors } = useColors();
+	return (
+		<Box width={'100%'}>
+			<Head>
+				<title>About Us</title>
+				<meta
+					name="description"
+					content="Leftovers is dedicated to providing a platform that allows neighbors to share their excess meals with others in the community"
+				/>
+			</Head>
+			<header>
+				<Box
+					width={'100%'}
+					height={{ xs: '35vh', sm: '40vh', md: '50vh' }}
+					display={'flex'}
+					backgroundColor={colors.primary[400]}
+					justifyContent="center"
+					alignItems={'center'}
+				>
+					<Box position={'relative'}>
+						<Typography
+							fontWeight={800}
+							color={'secondary'}
+							fontSize={{ xs: '3em', sm: '4em', md: '5em' }}
+							variant="h1"
+						>
+							Privacy Policy
+						</Typography>
+					</Box>
+				</Box>
+			</header>
+			<main>
+				<Box
+					m={5}
+					display={'flex'}
+					justifyContent={'center'}
+					mx={'1.5em'}
+					textOverflow={'clip'}
+				>
+					<PrivacyStatement />
+				</Box>
+			</main>
+			<footer className={styles.footer}>
+				<Footer img={'/icons8-connect.svg'} title="Leftovers" />
+			</footer>
+		</Box>
+	);
+};
+
+export default PrivacyPolicy;
