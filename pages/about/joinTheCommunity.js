2749a97a99cec4f2f53881ae170ec495264b3fee
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2023-05-27 21:25:46 -0700

    styling

--- pages/about/joinTheCommunity.js
+++ pages/about/joinTheCommunity.js
@@ -52,25 +52,49 @@ const joinTheCommunity = () => {
 				<Box
 					m={5}
 					display={'flex'}
+					flexDirection={'column'}
 					justifyContent={'center'}
+					alignItems={'center'}
 					mx={'1.5em'}
 					textOverflow={'clip'}
 				>
+					<Typography variant="h2">
+						What does it mean to be a prepper?
+					</Typography>
+					<br />
 					<Typography fontSize={'large'} component={'p'} sx={{ width: '50em' }}>
 						Velit sint laboris labore laboris dolore aute veniam quis. Id ex
 						pariatur incididunt consequat sunt ullamco id consequat. Adipisicing
 						qui cupidatat ipsum aliquip. Consectetur id ipsum anim ipsum do
 						laborum ad commodo ea cupidatat aute laborum consequat dolor.
-						<br />
-						<br />
+					</Typography>
+					<br />
+
+					<Typography fontSize={'large'} component={'p'} sx={{ width: '50em' }}>
 						Est aliquip ipsum magna excepteur sit duis occaecat ea cillum. Id
 						nisi elit reprehenderit aute deserunt ullamco cillum do consectetur
 						irure do. Cillum qui non amet ea elit ut. Occaecat qui aliqua non
 						labore in eiusmod reprehenderit. Ex magna nisi dolore culpa nisi
 						veniam aliqua duis. Consectetur quis do tempor sint consequat
 						consectetur dolor.
-						<br />
-						<br />
+					</Typography>
+					<br />
+					<Typography variant="h2">What is required</Typography>
+					<br />
+					<Typography fontSize={'large'} component={'p'} sx={{ width: '50em' }}>
+						Laboris id anim est sunt nulla sunt aliqua dolor eiusmod tempor
+						magna nulla quis duis. Est cupidatat duis ea nisi duis velit cillum
+						magna nostrud mollit do mollit. Dolor duis occaecat elit nisi nulla
+						amet enim commodo quis tempor. Nisi et mollit consectetur incididunt
+						laborum eu consectetur irure labore ex laboris dolore magna fugiat.
+						Irure commodo quis cupidatat officia officia cupidatat. Dolore nisi
+						officia voluptate incididunt cillum fugiat ad fugiat labore nulla
+						culpa consectetur exercitation.
+					</Typography>
+					<br />
+					<Typography variant="h2">Start Today!</Typography>
+					<br />
+					<Typography fontSize={'large'} component={'p'} sx={{ width: '50em' }}>
 						Laboris id anim est sunt nulla sunt aliqua dolor eiusmod tempor
 						magna nulla quis duis. Est cupidatat duis ea nisi duis velit cillum
 						magna nostrud mollit do mollit. Dolor duis occaecat elit nisi nulla
@@ -82,7 +106,7 @@ const joinTheCommunity = () => {
 					</Typography>
 				</Box>
 				<Box>
-					<PromoSection />
+					<PromoSection bgColor={colors.primary[400]} />
 					<PromoSection reverse />
 				</Box>
 			</main>
