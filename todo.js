ea8761d9491aff8a3d2cee113f56adc4944b2975
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-25 17:07:49 -0700

    updated todo list

--- todo.js
+++ todo.js
@@ -1,39 +1,20 @@
-//continue order status in modal. update "update_at" while changing status
-//do a check if prepperEmail matches current user to use modal to set status
-//cart items number count on icon
 //order details page
-//order item add kitchen name to db and meal item
 //checkout items list fix description alignment
 //style confirmation page
 //orders in myKitchen
 //order pagination or overflow
-//limit cart items to one single prepper
-//orders collection in mongodb
-//limit cart items and orders to one prepper only per order
 //pagination for front page lists
 //continue pagination. "load more preppers"
-//setTimeout on alert msg
 //mongodb schemas for users, preppers, meals etc.
-//put promo section in about sub page as well
-// about us pages and index page and icons
-//faq
-//limit orders to one prepper at a time
 //open and close store
 //prepper page. grab zipcode from user info if they have it. create setting to set a zipcode or use geolocation.
 //check category banner responsiveness. pagination options
 //front page signed in version and signed out version
 //messages
-//sidebar menuitem components refactor
-//prepper page header
 //meal pic in db
-//checkout page design
 //stripe
-//send image file to a img hosting server e.g. Cloudinary for my kitchen related pics
+//send image file to a img hosting server e.g. Cloudinary / uploadThing for my kitchen related pics
 //put url to that image in mealDetails to send to mongodb
 //custom error page for nextauth
-
-//handleAddMealForm
-//send image file to a img hosting server e.g. Cloudinary
-//put url to that image in mealDetails to send to mongodb
 
 //guard checks on all api endpoints
