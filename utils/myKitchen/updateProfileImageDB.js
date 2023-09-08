653b0598e2329f5ccb7e3dc1ec3b3ade83f0945f
Luis Gonzalez <lgonzalez23@verizon.net>
2023-09-07 17:30:09 -0700

    fetch util function to update profile image

---
+++ utils/myKitchen/updateProfileImageDB.js
@@ -0,0 +1,17 @@
+export async function updateKitchenImageDb(email, profileImgUrl, type) {
+	const formBody = {
+		profileImgUrl,
+		email,
+		type
+	};
+
+	const response = await fetch('/api/myKitchen/updateProfileImage', {
+		headers: {
+			'Content-type': 'application/json'
+		},
+		method: 'PATCH',
+		body: JSON.stringify(formBody)
+	});
+	const data = await response.json();
+	return data;
+}
