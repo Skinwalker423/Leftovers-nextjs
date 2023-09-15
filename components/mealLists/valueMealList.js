7e29f040278d76d99e33cec13e2a4cbd2aa4ec0d
Luis Gonzalez <lgonzalez23@verizon.net>
2023-09-14 17:33:42 -0700

    updated value meal list

--- components/mealLists/valueMealList.js
+++ components/mealLists/valueMealList.js
@@ -26,6 +26,7 @@ const ValueMealList = ({ userEmail, setMsg }) => {
 							image={image}
 							setMsg={setMsg}
 							isKitchenClosed={prepper.isKitchenClosed}
+							showClosed={true}
 						/>
 					);
 				}
