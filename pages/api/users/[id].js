ad8a5eb57a05747743044203aec07c765a91ff8f
Luis Gonzalez <lgonzalez23@verizon.net>
2023-09-22 16:42:29 -0700

    authorization to update zipcode

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,13 +1,20 @@
 import User from '../../../db/mongodb/models/userModel';
 import { connectToMongoDb } from '../../../db/mongodb/mongoose';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
 
 async function updateUserDefaultZipcode(req, res) {
 	if (req.method !== 'PATCH') {
 		return res.status(400).json({ error: 'invalid request' });
 	}
 
+	const session = await getServerSession(req, res, authOptions);
 	const { id } = req.query;
 	const newZipcode = req.body;
+
+	if (session?.user?.id !== id) {
+		return res.status(401).json({ error: 'Not authorized' });
+	}
 
 	try {
 		await connectToMongoDb();
@@ -17,7 +24,7 @@ async function updateUserDefaultZipcode(req, res) {
 			}
 		});
 		await updatedUser.save();
-		res.status(200).json({ message: 'update complete' });
+		res.status(200).json({ message: 'default zipcode updated' });
 	} catch (error) {
 		res.status(500).json({ error: 'problem updating existing user' });
 	}
