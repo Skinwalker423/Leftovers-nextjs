d0dc8577b0e14a33446d03606ea96036f2d2428f
Luis Gonzalez <lgonzalez23@verizon.net>
2023-09-24 17:45:27 -0700

    Map component

--- utils/GoogleMaps/googleMapsLoader.js
+++ utils/GoogleMaps/googleMapsLoader.js
@@ -1,7 +1,7 @@
 import { Loader } from '@googlemaps/js-api-loader';
 
 const loader = new Loader({
-	apiKey: process.env.GOOGLE_KEY,
+	apiKey: process.env.GOOGLE_API_KEY,
 	version: 'weekly',
 	libraries: ['places']
 });
