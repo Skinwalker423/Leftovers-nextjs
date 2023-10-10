b2e118ff9ccb6e5af79329d6093ce7415b5945e0
Luis Gonzalez <lgonzalez23@verizon.net>
2023-10-09 17:26:50 -0700

    OrderDetails

--- pages/myorders/[orderId]/index.js
+++ pages/myorders/[orderId]/index.js
@@ -1,22 +1,51 @@
 import React from 'react';
+import { Box } from '@mui/material';
 import OrderCard from '../../../components/myKitchen/orders/orderCard';
 import { fetchOrderById } from '../../../db/mongodb/mongoose';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../api/auth/[...nextauth]';
 
-export async function getServerSideProps({ params }) {
+export async function getServerSideProps({ req, res, params }) {
 	const orderId = params.orderId;
+	const session = await getServerSession(req, res, authOptions);
+
+	const userEmail = session?.user?.email;
+
+	console.log('session', session);
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/signin',
+				permanent: false
+			}
+		};
+	}
+
 	const order = await fetchOrderById(orderId);
 	console.log('order in ssr', order);
 
+	if (userEmail !== order.userEmail) {
+		return {
+			notFound: true
+		};
+	}
+
 	return {
 		props: {
-			order
+			order,
+			userEmail
 		}
 	};
 }
 
-const OrderDetails = ({ order }) => {
+const OrderDetails = ({ order, userEmail }) => {
 	console.log('order', order);
-	return <div>OrderDetails</div>;
+	return (
+		<Box>
+			<OrderCard order={order} currentUserEmail={userEmail} />
+		</Box>
+	);
 };
 
 export default OrderDetails;
