c8e5c6c2fae7b862e322d9e39d3b1105216ba13b
Luis Gonzalez <lgonzalez23@verizon.net>
2023-10-09 17:33:06 -0700

    myOrderList

---
+++ components/myKitchen/orders/myOrdersList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+const OrdersList = ({ myOrders, currentUserEmail }) => {
+	return (
+		<Box
+			display="flex"
+			flexDirection={'column'}
+			justifyContent={'center'}
+			alignItems={'center'}
+			height={'100%'}
+			width={{ xs: '100%', sm: '90%', md: '80%' }}
+			ml={{ xs: 0, sm: 7 }}
+			p={3}
+			gap={5}
+		>
+			{myOrders.length > 0 &&
+				myOrders.map((order) => {
+					return (
+						<MyOrderCard
+							key={order.id}
+							order={order}
+							currentUserEmail={currentUserEmail}
+						/>
+					);
+				})}
+		</Box>
+	);
+};
+
+export default OrdersList;
