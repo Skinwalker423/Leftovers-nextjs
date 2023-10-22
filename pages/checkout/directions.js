7386bc2798c8fe3343e4f8dbe5d41b95a65c1d46
Luis Gonzalez <lgonzalez23@verizon.net>
2023-10-21 18:31:32 -0700

    remove mapbox

--- pages/checkout/directions.js
+++ pages/checkout/directions.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import Map from '../../components/checkout/map';
 
 const directions = () => {
-	return (
-		<div>
-			<Map />
-		</div>
-	);
+	return <div>Map</div>;
 };
 
 export default directions;
