e72ced5d21a070262288a8f9306171624378522a
Luis Gonzalez <lgonzalez23@verizon.net>
2023-10-23 18:37:21 -0700

    swiper js test

--- pages/checkout/directions.js
+++ pages/checkout/directions.js
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Box } from '@mui/material';
 
-const directions = () => {
-	return <div>Map</div>;
-};
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default directions;
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Keyboard, Scrollbar, Navigation, Pagination } from 'swiper/modules';
+
+export default function Directions() {
+	return (
+		<Box
+			display={'flex'}
+			width={'100%'}
+			height={'100vh'}
+			justifyContent={'center'}
+			alignItems={'center'}
+		>
+			<Box height={'200px'} width={'80%'}>
+				<Swiper
+					slidesPerView={1}
+					centeredSlides={false}
+					slidesPerGroupSkip={4}
+					grabCursor={true}
+					keyboard={{
+						enabled: true
+					}}
+					breakpoints={{
+						769: {
+							slidesPerView: 2,
+							slidesPerGroup: 2
+						}
+					}}
+					scrollbar={true}
+					navigation={true}
+					pagination={{
+						clickable: true
+					}}
+					modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+					className="mySwiper"
+				>
+					<SwiperSlide>
+						<img src="https://cdn.magloft.com/github/swiper/images/page-001.jpg" />
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src="https://cdn.magloft.com/github/swiper/images/page-002.jpg" />
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src="https://cdn.magloft.com/github/swiper/images/page-003.jpg" />
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src="https://cdn.magloft.com/github/swiper/images/page-004.jpg" />
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src="https://cdn.magloft.com/github/swiper/images/page-005.jpg" />
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src="https://cdn.magloft.com/github/swiper/images/page-006.jpg" />
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src="https://cdn.magloft.com/github/swiper/images/page-007.jpg" />
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src="https://cdn.magloft.com/github/swiper/images/page-008.jpg" />
+					</SwiperSlide>
+					<SwiperSlide>
+						<img src="https://cdn.magloft.com/github/swiper/images/page-009.jpg" />
+					</SwiperSlide>
+				</Swiper>
+			</Box>
+		</Box>
+	);
+}
 
 // import Head from 'next/head';
 // import Image from 'next/image';
