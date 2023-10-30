5d413dfd8db4172e1675a7e9e30e05dad5a2c22b
Luis Gonzalez <lgonzalez23@verizon.net>
2023-10-29 22:14:24 -0700

    notification list

--- components/notifications/notificationMenu.js
+++ components/notifications/notificationMenu.js
@@ -3,8 +3,11 @@ import { Box, Typography, Paper } from '@mui/material';
 import NotificationList from './notificationList';
 import Link from 'next/link';
 import { useColors } from '../../hooks/useColors';
+import { mockDataContacts } from '../../db/mockData';
 
 const NotificationMenu = () => {
+	const devList =
+		process.env.NEXT_PUBLIC_DEVELOPMENT_MODE === 'true' ? mockDataContacts : [];
 	const { colors } = useColors();
 	return (
 		<Paper
@@ -14,20 +17,22 @@ const NotificationMenu = () => {
 				right: '4%',
 				zIndex: 99,
 				width: '500px',
-				height: '500px',
-			}}>
+				height: '500px'
+			}}
+		>
 			<Box py={'1em'} backgroundColor={colors.gray[700]}>
-				<Typography variant='h2' textAlign={'center'}>
+				<Typography variant="h2" textAlign={'center'}>
 					Messages
 				</Typography>
 			</Box>
-			<NotificationList />
-			<Box py='1em' backgroundColor={colors.gray[400]}>
+			<NotificationList list={devList} />
+			<Box py="1em" backgroundColor={colors.gray[400]}>
 				<Link style={{ textDecoration: 'none' }} href={'/messages'}>
 					<Typography
 						color={colors.orangeAccent[900]}
-						variant='h3'
-						textAlign={'center'}>
+						variant="h3"
+						textAlign={'center'}
+					>
 						See all
 					</Typography>
 				</Link>
