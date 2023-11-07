c9c3504ef5fb956df104b3490530db881acea97b
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-06 20:13:40 -0800

    user update token values

--- utils/mailer/mailer.ts
+++ utils/mailer/mailer.ts
@@ -1,2 +1,48 @@
 import nodemailer from 'nodemailer';
 import bcrypt from 'bcrypt';
+import User from '../../db/mongodb/models/userModel';
+import { connectToMongoDb } from '../../db/mongodb/mongoose';
+
+enum EmailTypes {
+	RESET = 'RESET',
+	VERIFY = 'VERIFY'
+}
+
+export const sendEmail = async ({
+	email,
+	emailType,
+	userId
+}: {
+	email: string;
+	emailType: EmailTypes;
+	userId: string;
+}) => {
+	try {
+		const hashedToken = bcrypt.hash(userId.toString(), 10);
+
+		await connectToMongoDb();
+
+		if (emailType === 'VERIFY') {
+			await User.findByIdAndUpdate(userId, {
+				verifyToken: hashedToken,
+				verifyTokenExpiry: Date.now() + 3600000
+			});
+		} else if (emailType === 'RESET') {
+			await User.findByIdAndUpdate(userId, {
+				forgotPasswordToken: hashedToken,
+				forgotPasswordTokenExpiry: Date.now() + 3600000
+			});
+		}
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
+
+	let transport = nodemailer.createTransport({
+		host: process.env.MAILTRAP_HOST,
+		port: 2525,
+		auth: {
+			user: process.env.MAILTRAP_USER,
+			pass: process.env.MAILTRAP_PASS
+		}
+	});
+};
