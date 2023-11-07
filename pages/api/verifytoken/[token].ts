8b5d695c7ac3336786f3f8cea5c398ba0707404a
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-06 21:14:15 -0800

    verifyToken api

--- pages/api/verifytoken/[token].ts
+++ pages/api/verifytoken/[token].ts
@@ -6,22 +6,27 @@ const verifyToken = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { token } = req.query;
 	if (!token) return res.status(400).json({ error: 'No token found' });
 	console.log('token', token);
-	await connectToMongoDb();
-	const foundUser = await User.findOne({
-		verifyToken: token,
-		verifyTokenExpiry: { $gt: Date.now() }
-	});
-
-	if (!foundUser) return res.status(400).json({ error: 'User not found' });
-
-	console.log('found user', foundUser);
-
 	try {
+		await connectToMongoDb();
+		const foundUser = await User.findOne({
+			verifyToken: token,
+			verifyTokenExpiry: { $gt: Date.now() }
+		});
+
+		if (!foundUser) return res.status(400).json({ error: 'User not found' });
+
+		console.log('found user', foundUser);
+
+		foundUser.isVerified = true;
+		foundUser.verifyToken = undefined;
+		foundUser.verifyTokenExpiry = undefined;
+
+		await foundUser.save();
+
+		return res.status(200).json({ message: 'Successfully verified Email' });
 	} catch (error: any) {
 		return res.status(500).json({ error: error.message });
 	}
-
-	res.status(200).json(token);
 };
 
 export default verifyToken;
