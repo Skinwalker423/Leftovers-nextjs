2483bebd463cac04f6c6528937611ea31e8a7a8d
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-11 19:24:35 -0800

    success/error alerts

--- pages/onboarding/[userId].js
+++ pages/onboarding/[userId].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import User from '../../db/mongodb/models/userModel';
 import { connectToMongoDb } from '../../db/mongodb/mongoose';
 import { Alert, Box, Button, Typography } from '@mui/material';
@@ -28,7 +28,9 @@ export async function getServerSideProps({ req, res, params: { userId } }) {
 		const formattedUser = {
 			id: newUser._id.toString(),
 			email: newUser.email,
-			isVerified: newUser?.isVerified
+			isVerified: newUser?.isVerified,
+			image: newUser?.image || '',
+			name: newUser?.name || ''
 		};
 
 		return {
@@ -38,17 +40,16 @@ export async function getServerSideProps({ req, res, params: { userId } }) {
 		};
 	} catch (error) {
 		return {
-			props: {
-				error: error.message
-			}
+			notFound: true
 		};
 	}
 }
 
-const onboarding = ({ error, user }) => {
-	if (error) return error;
-
+const onboarding = ({ user }) => {
 	console.log('user', user);
+
+	const [msg, setMsg] = useState('');
+	const [error, setError] = useState('');
 
 	const handleVerifyEmail = async () => {
 		console.log('sending verification email');
@@ -59,31 +60,37 @@ const onboarding = ({ error, user }) => {
 		});
 		if (emailRes.error) {
 			console.log('problem with sending email', emailRes.error);
+			setError(emailRes.error);
 		} else {
 			console.log('res message', emailRes.message);
+			setMsg(emailRes.message);
 		}
 	};
 
 	return (
-		<Box width={'100%'} px={20}>
-			<Typography variant="h1">Onboarding</Typography>
-			{user?.isVerified ? (
-				<Alert
-					sx={{
-						width: '100%',
-						fontSize: 'larger'
-					}}
-					severity="success"
-				>
-					Already Verified
-				</Alert>
-			) : (
-				<Box>
-					<Button variant="success" onClick={handleVerifyEmail}>
-						Verify Email
-					</Button>
-				</Box>
-			)}
+		<Box width={'100%'} px={20} pt={5}>
+			<Box>
+				<Typography variant="h1">Onboarding</Typography>
+				{user?.isVerified ? (
+					<Alert
+						sx={{
+							width: '100%',
+							fontSize: 'larger'
+						}}
+						severity="success"
+					>
+						Already Verified
+					</Alert>
+				) : (
+					<Box>
+						<Button variant="success" onClick={handleVerifyEmail}>
+							Verify Email
+						</Button>
+					</Box>
+				)}
+			</Box>
+			{msg.length > 0 && <SuccessAlert msg={msg} setMsg={setMsg} />}
+			{error.length > 0 && <ErrorAlert error={error} setError={setError} />}
 		</Box>
 	);
 };
